3e8ccded1ebd31a72ec215463d0f5760a85baed6
ThaliaTritar <161451235+ThaliaTritar@users.noreply.github.com>
2025-02-19 17:23:53 +0100

    Fixed several bugs that came with implementing CMake.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,151 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
 
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+
+// Validation layers or not (based on debug mode or not)
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    // Functions
+    //-----------
+
+    void initWindow() {
+        // 1. Initialize the GLFW library.
+        //-----
+        glfwInit();
+        
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // disable resized windows for now
+
+        // 2. Create the window.
+        //-----
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+        createInstance();
+
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+
+
+        vkDestroyInstance(instance, nullptr);
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();    
+    }
+
+
+    void createInstance() {
+
+       // // Validation layer
+       if (enableValidationLayers && !checkValidationLayerSupport()) {
+           throw std::runtime_error("validation layers requested, but not available!");
+       }
+
+
+
+        // 1. Create appInfo struct (optional)
+        //------------------------------------
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+        // 2. Create createInfo struct
+        //-----------------------------
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        // Extension support
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+       
+        // 3. Create instance
+        //--------------------
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+    bool checkValidationLayerSupport() {
+       uint32_t layerCount;
+       vkEnumerateInstanceLayerProperties(&layerCount, nullptr);   // list all of the available layers
+      
+       std::vector<VkLayerProperties> availableLayers(layerCount);
+       vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+      
+       return false;
+    }
+
+    // Datamembers
+    //-----
+    GLFWwindow* window;
+    VkInstance instance;
+
+};
 
 int main() {
-   glfwInit();
+    HelloTriangleApplication app;
 
-   std::cout << "Hello, Vulkan!" << std::endl;
-   
-   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-   GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-   
-   uint32_t extensionCount = 0;
-   vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-   
-   glm::mat4 matrix;
-   glm::vec4 vec;
-   auto test = matrix * vec;
-   
-   while (!glfwWindowShouldClose(window)) {
-       glfwPollEvents();
-   }
-   
-   glfwDestroyWindow(window);
-   
-   glfwTerminate();
-   
-   return 0;
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
